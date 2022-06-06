cfec79d83fdc4d20687d91f195d80ccfcca4f21a
RominM <rominmanogil.off@gmail.com>
2022-06-06 20:04:04 +0200

    feat: :construction: init Routing : Add all routes

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Crew from '../pages/Crew';
+import Destination from '../pages/Destination';
 import Home from '../pages/Home';
+import Technology from '../pages/Technology';
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
       </Routes>
     </Router>
   );
